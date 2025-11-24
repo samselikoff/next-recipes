14d06245f79cdaacb9c33a0d5d35c575e02f0faa
Sam Selikoff <sam.selikoff@gmail.com>
2025-11-24 18:13:47 -0500

    Sample summary

--- app/(app)/(recipes)/basic-data-fetching/page.tsx
+++ app/(app)/(recipes)/basic-data-fetching/page.tsx
@@ -1,3 +1,4 @@
+import dedent from "dedent";
 import { Frame } from "../_components/frame";
 
 export default function Page() {
@@ -12,6 +13,50 @@ export default function Page() {
       </p>
 
       <Frame src="/demos/basic-data-fetching" />
+
+      <div className="prose my-20">
+        <p>
+          In Next.js 16, calling async functions in Server Components is the
+          primary way you fetch data:
+        </p>
+
+        <pre>
+          {dedent`
+            export async function Page() {
+              await getPosts()
+
+              // ...
+            }
+          `}
+        </pre>
+
+        <p>
+          With Cache Components, Next will ensure that your data-fetching
+          components don&apos;t fully block your routes by enforcing that you
+          provide some static fallback content with Suspense:
+        </p>
+
+        <pre>
+          {dedent`
+            export async function Page() {
+              return (
+                <Suspense fallback={<Spinner />}>
+                  <Posts />
+                </Suspense>
+              )
+            }
+          `}
+        </pre>
+
+        <p>
+          This gives your app a fast initial boot—as fast as you&apos;d get with
+          a traditional SSG or JAMstack approach, since it can be served from a
+          CDN—while <em>still</em> getting to fetch your dynamic data on the
+          server as part of the initial request.
+        </p>
+
+        <p>No client-side data fetching library or API routes needed.</p>
+      </div>
     </div>
   );
 }
--- app/(app)/(recipes)/layout.tsx
+++ app/(app)/(recipes)/layout.tsx
@@ -1,5 +1,5 @@
 import { ReactNode } from "react";
 
 export default function Layout({ children }: { children: ReactNode }) {
-  return <div className="mx-auto mt-8 max-w-2xl">{children}</div>;
+  return <div className="mx-auto mt-20 max-w-2xl">{children}</div>;
 }
